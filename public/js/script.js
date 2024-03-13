396bcac21f06285f3915ba70436b42c68022c7f3
bruno requel <brunoprograma2004@gmail.com>
2024-03-13 20:24:02 -0300

    otras cosas

--- public/js/script.js
+++ public/js/script.js
@@ -96,41 +96,6 @@ searchInput.addEventListener("change", () => {
 
 
 
-// CARRUSEL FRASES
-const carousel = document.getElementById('carousel');
-let counter = 1;
-const size = carousel.children[0].clientWidth;
-carousel.style.transform = `translateX(${-size * counter}px)`;
-
-function moveCarousel(direction) {
-  if (direction === 'prev') {
-    counter--;
-  } else if (direction === 'next') {
-    counter++;
-  }
-  carousel.style.transition = 'transform 0.5s ease-in-out';
-  carousel.style.transform = `translateX(${-size * counter}px)`;
-}
-
-carousel.addEventListener('transitionend', () => {
-  if (carousel.children[counter].id === 'firstClone') {
-    carousel.style.transition = 'none';
-    counter = carousel.children.length - 4;
-    carousel.style.transform = `translateX(${-size * counter}px)`;
-  } else if (carousel.children[counter].id === 'lastClone') {
-    carousel.style.transition = 'none';
-    counter = carousel.children.length - counter;
-    carousel.style.transform = `translateX(${-size * counter}px)`;
-  }
-});
-
-// Clonar primer y último elemento
-
-// Auto mover el carrusel
-setInterval(() => {
-  moveCarousel('next');
-}, 15000);
-// CARRUSEL FRASES
 
 
 
